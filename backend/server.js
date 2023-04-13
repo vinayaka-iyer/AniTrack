ce2e071d240320f16ee4748698d8e9f4c715ae39
vinayaka-iyer <vinayakaiyer999@gmail.com>
2023-04-13 15:09:30 +0530

    Initialized Frontend and Backend

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const PORT = 8000;
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => console.log("server running on PORT " + PORT));
